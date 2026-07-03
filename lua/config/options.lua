4cb4ccc8a937dbe4aca9d27b87c9c448c97b2fc5
achen2089 <anthony.chen@zettabyte.space>
2026-07-03 12:38:47 -0700

    fix: parse jsonc with json parser (jsonc dropped from nvim-treesitter main)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- nvim-treesitter (main branch) dropped the jsonc parser; parse jsonc with json
+vim.treesitter.language.register("json", "jsonc")
